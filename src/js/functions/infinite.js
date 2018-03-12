61cdc23439e298bbca1204cba980c4e0f1abeee5
Gilberto Velez <gvelezjr02@gmail.com>
2018-03-12 15:44:24 -0400

    wip infinite module

--- src/js/functions/infinite.js
+++ src/js/functions/infinite.js
@@ -1,10 +1,18 @@
-const infiniteSlide = slide => {
+const infiniteSlide = ( direction, index, slide ) => {
 
 	let clone = slide.cloneNode( true );
 
 	clone.classList.add( 'blicky-cloned' );
 
-	clone.style.cssText = 'display: none';
+	if ( direction == 'left' ) {
+
+		clone.dataset.blickyIndex = index - 1;
+
+	} else {
+
+		clone.dataset.blickyIndex = index + 1;
+
+	}
 
 	return clone;
 
@@ -16,25 +24,21 @@ export function infinite() {
 
 	let slides = this.slider.slides;
 
-	let half = Math.ceil( this.slider.slideCount / 2 );
-
 	slides
 
-		.slice( 0, half )
+		.slice( slider.slideCount - 1, slider.slideCount )
 
 		.reverse()
 
-		.forEach( slide => slider.blicky.insertBefore( infiniteSlide( slide ), slider.blicky.children[0] ) )
+		.forEach( ( slide, index ) => slider.blicky.insertBefore( infiniteSlide( 'left', index, slide ), slider.blicky.children[0] ) )
 
 	;
 
 	slides
 
-		.slice( half - 1, slider.slideCount )
+		.slice( 0, 1 )
 
-		.reverse()
-
-		.forEach( slide => slider.blicky.appendChild( infiniteSlide( slide ) ) )
+		.forEach( ( slide, index ) => slider.blicky.appendChild( infiniteSlide( 'right', index, slide ) ) )
 
 	;
 
