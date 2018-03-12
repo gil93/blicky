e4c635ea14cc96824cfd0a910eff1b0e87d8467d
Gilberto Velez <gvelezjr02@gmail.com>
2018-03-12 15:45:12 -0400

    wip build module

--- src/js/functions/build.js
+++ src/js/functions/build.js
@@ -3,9 +3,13 @@ import { move } from './move';
 
 export function build() {
 
-	this.slider.blicky.style.width = `${this.slider.width * this.slider.slideCount}px`;
+	let domSlides = this.slider.blicky.getElementsByClassName( 'blicky-slide' );
 
-	this.slider.slides.forEach( slide => slide.style.width = `${this.slider.width}px` );
+	this.slider.blicky.style.width = `${this.slider.width * domSlides.length }px`;
+
+	Array.from( domSlides ).forEach( slide => slide.style.width = `${this.slider.width}px` );
+
+	this.slider.blicky.style.left = `${window.parseInt(domSlides[0].dataset.blickyIndex) * this.slider.width}px`;
 
 	this.slider.container.style.height = `${this.slider.height}px`;
 
@@ -17,11 +21,13 @@ export function rebuild() {
 
 		let newWidth = this.slider.element.parentElement.clientWidth;
 
+		let domSlides = this.slider.blicky.getElementsByClassName( 'blicky-slide' );
+
 		this.slider.blicky.style.width = `${newWidth * this.slider.slideCount}px`;
 
-		this.slider.slides.forEach( slide => slide.style.width = `${newWidth}px` );
+		Array.from( domSlides ).forEach( slide => slide.style.width = `${newWidth}px` );
 
-		this.slider.container.style.height = `${this.slider.slides[0].clientHeight}px`;
+		this.slider.container.style.height = `${domSlides[0].clientHeight}px`;
 
 		this.slider.width = this.slider.element.clientWidth;
 
